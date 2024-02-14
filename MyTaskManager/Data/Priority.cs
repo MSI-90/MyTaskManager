28346551f22f32135b579b2a27dce757594457bd
MSI-90 <104754352+MSI-90@users.noreply.github.com>
2024-02-15 00:29:25 +0700

    Added Entities for tables of the Fdata base, DbContext class is added also

---
+++ MyTaskManager/Data/Priority.cs
@@ -0,0 +1,9 @@
+﻿namespace MyTaskManager.Data
+{
+    public class Priority
+    {
+        public string? Name { get; set; }
+        public string? Descrption { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
---
+++ MyTaskManager/Data/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace MyTaskManager.Data
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string? TitleTask { get; set; }
+        public Category? CategoryId { get; set; }
+        public Priority? PrioryName { get; set; }
+        public DateTime Expiration { get; set; }
+        public User? UserId { get; set; }
+    }
+}
