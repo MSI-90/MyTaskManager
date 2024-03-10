5d90f083a52f19f87dc86b52c02993f595deb75d
MSI-90 <104754352+MSI-90@users.noreply.github.com>
2024-03-10 12:05:45 +0700

    1. Добавлены модели DTO LoginRequestDTO, LoginResponseDTO, RegisterationReuestDTO. Данные модели будут выступать в качестве вспомогательных в реализации функционала Аутентификации и Идентификации пользователя. 2. Содан класс UserRepository, реализующий интерфейс IUserRepository. Данный класс содержит 3 метода, проверяет наличие польщователя в БД,, регистрация нового пользователя, аутентификация существующего пользователя

--- MyTaskManager/Repositories/UserRepository.cs
+++ MyTaskManager/Repositories/UserRepository.cs
@@ -1,16 +1,25 @@
-﻿using Models.EfClasses;
+﻿using Microsoft.IdentityModel.Tokens;
+using Models.EfClasses;
 using MyTaskManager.EfCode;
 using MyTaskManager.Models.DTO.User.AuthDTO;
 using MyTaskManager.Models.DTO.User.RegistrationDTO;
 using MyTaskManager.Models.DTO.UserDTO;
 using MyTaskManager.Repositories.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MyTaskManager.Repositories
 {
     public class UserRepository : IUserRepository
     {
         private readonly TaskContext _taskContext;
-        public UserRepository(TaskContext taskContext) => _taskContext = taskContext;
+        private readonly string _secretKey = "";
+        public UserRepository(TaskContext taskContext, IConfiguration configuration)
+        {
+            _taskContext = taskContext;
+            _secretKey = configuration.GetValue<string>("JWT:Secret", _secretKey);
+        }
         public bool IsUniqueUser(string username)
         {
             var user = _taskContext.Users.FirstOrDefault(u => u.UserName == username);
@@ -22,7 +31,41 @@ namespace MyTaskManager.Repositories
 
         public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
-            throw new NotImplementedException();
+            var user = _taskContext.Users.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower() &&
+            u.Password == loginRequestDTO.Password);
+
+            if (user == null)
+                return null;
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+
+            var authJwtClaims = new List<Claim>
+            {
+                new (JwtRegisteredClaimNames.Sub, user.UserName),
+                new (JwtRegisteredClaimNames.Email, user.Email),
+                new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            authJwtClaims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+            authJwtClaims.Add(new Claim(ClaimTypes.Role, user.Role));
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(authJwtClaims),
+                Expires = DateTime.UtcNow.AddDays(3),
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+            };
+
+            return loginResponseDTO;
         }
 
         public async Task<User> Register(RegisterationRequestDTO registerationRequestDTO)
